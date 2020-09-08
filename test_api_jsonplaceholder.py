107e314b16fee0a95034dd55a88b2f0591f5c3dc
varvarkrut <vanya.shcurko@gmail.com>
2020-09-08 12:50:09 +0300

    added correct tests.

--- test_api_jsonplaceholder.py
+++ test_api_jsonplaceholder.py
@@ -16,32 +16,22 @@ def test_get_post():
     assert (response.json() == correct_response_json), "The response is not correct"
 
 
-def test_get_comment():
+def test_get_post_incorrect_url():
+    response = requests.get(
+        'https://jsonplaceholder.typicode.com/posts/3636%2')
+    assert (response.status_code == 404), "The response is not correct"
+
+
+def test_get_null_post():
     json_correct_string = r"""
-    {"postId": 1,
-    "id": 1,
-    "name": "id labore ex et quam laborum",
-    "email": "Eliseo@gardner.biz",
-    "body": "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"}
+    {}
     """
     correct_response_json = json.loads(json_correct_string)
-    response = requests.get('https://jsonplaceholder.typicode.com/comments/1')
+    response = requests.get('https://jsonplaceholder.typicode.com/posts/0')
     assert (response.json() == correct_response_json), "The response is not correct"
 
 
-def test_get_album():
-    json_correct_string = r"""
-    {"userId": 1,
-    "id": 1,
-    "title":"quidem molestiae enim"
-    }
-    """
-    correct_response_json = json.loads(json_correct_string)
-    response = requests.get('https://jsonplaceholder.typicode.com/albums/1')
-    assert (response.json() == correct_response_json), "The response is not correct"
-
-
-def test_post_post():
+def test_post_valid_post():
     data = {'title': 'foo', 'body': 'bar', 'userId': 1, 'id': 101}
     response = requests.post(
         'https://jsonplaceholder.typicode.com/posts',
@@ -49,23 +39,22 @@ def test_post_post():
     assert (response.json() == data), "The response is not correct"
 
 
-def test_post_comment():
+def test_post_invalid_post():
     data = {
-        "name": "foo",
-        "emal": "bar",
-        "body": "test",
-        "id": 501,
-        "postID": 1}
+        "title": "foo",
+        "body": "bar",
+        "userId": 10,
+        "id": ""}
     response = requests.post(
-        'https://jsonplaceholder.typicode.com/comments',
+        'https://jsonplaceholder.typicode.com/posts',
         json=data)
-    assert (response.json() == data), "The response is not correct"
+    assert (response.status_code ==
+            400), f"Was recieved {response.status_code} code, while 400 expected"
 
 
-def test_post_album():
-    data = {"userID": 10, "id": 101, "title": "foo"}
+def test_post_empty_post():
+    data = {}
     response = requests.post(
-        'https://jsonplaceholder.typicode.com/albums',
+        'https://jsonplaceholder.typicode.com/posts',
         json=data)
-    assert (response.json() == data), "The response is not correct"
-
+    assert (response.json() == {'id': 101}), "The response is not correct"
